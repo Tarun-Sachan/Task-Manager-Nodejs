bb2cbb8e635c047817f62faf7a0554d6cffbde80
Tarun <tarunsachan1996@gmail.com>
2023-09-03 23:16:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+//middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+PORT =  3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    console.log("Connected to Db.....");
+    app.listen(PORT, console.log(`Listening on port ${PORT}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
